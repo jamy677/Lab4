8591e50dcc23757311c15b981bb5550212c59020
Aryan Jamwal <137585537+jamy677@users.noreply.github.com>
2024-11-19 21:54:08 -0500

    completing all the steps.

--- js/json.js
+++ js/json.js
@@ -17,3 +17,60 @@ async function populate() {
 
         // STEP 8: Output the iScream JSON object to the console 
         console.log(jsonObj);
+        // STEP 9a: Invoke the populateHeader function here, then build it below
+        populateHeader(jsonObj);
+
+        // STEP 10a: Invoke the showTopFlavors function here, then build it below
+        showTopFlavors(jsonObj);
+    } else {
+        console.error("Error fetching JSON data.");
+    }
+}
+
+// STEP 9b: Build out the populateHeader() function
+function populateHeader(jsonObj) {
+    const companyName = jsonObj.companyName;
+    header.textContent = companyName;
+}
+
+// STEP 10b: Assemble the showTopFlavors() function
+function showTopFlavors(jsonObj) {
+    const topFlavors = jsonObj.topFlavors;
+    
+    topFlavors.forEach(flavor => {
+        // STEP 10e: Build HTML elements for the content
+        const article = document.createElement('article');
+        const h2 = document.createElement('h2');
+        const pCalories = document.createElement('p');
+        const pType = document.createElement('p');
+        const ul = document.createElement('ul');
+        const img = document.createElement('img');
+
+        // STEP 10f: Set the textContent property for each of the above elements (except the UL)
+        h2.textContent = flavor.name;
+        pCalories.textContent = `Calories: ${flavor.calories}`;
+        pType.textContent = `Type: ${flavor.type}`;
+        img.src = `images/${flavor.image}`;
+        img.alt = flavor.name;
+
+        // STEP 10g: Build a loop for the ingredients array in the JSON
+        flavor.ingredients.forEach(ingredient => {
+            const li = document.createElement('li');
+            li.textContent = ingredient;
+            ul.appendChild(li);
+        });
+
+        // STEP 10h: Append each of the above HTML elements to the ARTICLE element
+        article.appendChild(h2);
+        article.appendChild(pCalories);
+        article.appendChild(pType);
+        article.appendChild(img);
+        article.appendChild(ul);
+
+        // STEP 10i: Append each complete ARTICLE element to the SECTION element
+        section.appendChild(article);
+    });
+}
+
+// STEP 3b: Call the populate() function
+populate();
